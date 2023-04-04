06761d6ad41668dd54e135f71531a6c95f376362
Akhil Kumar <akhilguretia@gmail.com>
2023-04-04 15:48:48 +0530

    Addinn Filtering feature for focused viewing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import { useState } from "react";
 import "./index.css";
 import ExpenseList from "./Components/ExpenseList";
-
+import ExpenseFilter from "./Components/ExpenseFilter";
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [expenses, setexpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "utility" },
-    { id: 2, description: "bbb", amount: 10, category: "utility" },
-    { id: 3, description: "ccc", amount: 10, category: "utility" },
-    { id: 4, description: "ddd", amount: 10, category: "utility" },
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
   ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((expenses) => expenses.category === selectedCategory)
+    : expenses;
 
   return (
     <div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
       <ExpenseList
-        expenses={expenses}
+        expenses={visibleExpenses}
         onDelete={(id) =>
           setexpenses(expenses.filter((item) => item.id !== id))
         }
